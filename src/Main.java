7b4a8d9f094732968d05193b29b5017820722518
Diego Vieira <dvdiego87@gmail.com>
2026-04-01 03:41:59 -0300

    criei as classes e implementei heranca e uma interface

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+         Video video[] = new Video[3];
+         video[0] = new Video("Aula de POO");
+         video[1] = new Video("Aula de Java");
+         video[2] = new Video("Aula de JavaScript");
 
+         Gafanhoto gafanhoto[] = new Gafanhoto[2];
+         gafanhoto[0] = new Gafanhoto("João", 22, "M", "Jão123");
+         gafanhoto[1] = new Gafanhoto("Maria", 30, "F", "Maria123");
+
+        System.out.println(video[0].toString());
+        System.out.println(gafanhoto[0].toString());
     }
 }
